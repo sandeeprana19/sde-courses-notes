5cd1e1661e53c16ee501da5b0cf564665e114f64
Sandeep Rana <sandeeprana.or.sr@gmail.com>
2026-07-16 20:15:20 +0530

    Updated: 12. Define debounce?
    Updated: 12. Define debounce?

--- src/components/Interview/React.jsx
+++ src/components/Interview/React.jsx
@@ -896,22 +896,28 @@ const React = () => {
                   <ol className="list-[lower-roman] pl-5 flex flex-col gap-y-2">
                     <li>
                       <p>
-                        Debouncing delays a function's execution until a pause
-                        occurs since the last event for example, let's say
-                        waiting 200ms after the last keystroke before firing an
-                        API call. In React, I implement this by binding the
-                        search input's value to a state variable and calling the
-                        API inside a useEffect hook with that state variable in
-                        the dependency array. After each keystroke, the state
+                        Debouncing delays a function's execution until there is
+                        a pause occurs since the last event for example, instead
+                        of calling a API on every keystroke I wait 200ms after
+                        user stop typing before making the API call.
+                      </p>
+                      <p>
+                        In React, I implement this by binding the search input
+                        field value to a state variable and calling the search
+                        API inside useEffect hook with that state variable in
+                        the dependency array. So after each keystroke, the state
                         updates and triggers React's reconciliation algorithm
                         process where it re-renders the component and then since
-                        the dependency changed, React first runs the previous
+                        the dependency changed React first runs the previous
                         useEffect's cleanup function where I call clearTimeout
-                        to clear the old timer & then runs the useEffect
-                        callback function again which starts a fresh setTimeout.
+                        function to clear the old timer & then runs the
+                        useEffect callback function again which starts a fresh
+                        setTimeout function.
+                      </p>
+                      <p>
                         And then let's say if there are no keystroke happens
-                        from user's end before 200ms pass then it actually fires
-                        the API call.
+                        from user's end before 200ms passed then I actually
+                        makes the API call.
                       </p>
                     </li>
                     <li>
